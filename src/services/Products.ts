064deb07b279c1e444ec65b2da0352813fa4a638
parsgeorg <parsegovgeorgiy@gmail.com>
2020-05-31 16:29:38 +0300

    Update Products.ts

--- src/services/Products.ts
+++ src/services/Products.ts
@@ -16,24 +16,4 @@ export const getProductById = async (id: string) => {
   return products.filter(product => product.caseUid === id)[0];
 };
 
-// export function PostData(type, userData) {
-//     let BaseURL = '../testData';
-
-//     return new Promise((resolve, reject) =>{
-
-//         fetch(BaseURL+type, {
-//             method: 'POST',
-//             body: JSON.stringify(userData)
-//           })
-//           .then((response) => response.json())
-//           .then((res) => {
-//             resolve(res);
-//           })
-//           .catch((error) => {
-//             reject(error);
-//           });
-
-//       });
-// }
-
 export type Product = typeof products[0];
